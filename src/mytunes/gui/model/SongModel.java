7f86ba5a98a53c075b0666f9c4d894b457ca4ac1
Radoslav Backovsky <mr.backy@gmail.com>
2019-11-29 13:44:38 +0100

    comment deletion

--- src/mytunes/gui/model/SongModel.java
+++ src/mytunes/gui/model/SongModel.java
@@ -11,15 +11,12 @@ public class SongModel {
     ArrayList<Song> songList = new ArrayList<>();
     private ObservableList<Song> allSongs = FXCollections.observableArrayList();
     
- 
     public SongModel() {
         songList.add(new Song("Stayin Out All Night", "Wiz Khalifa", 29, "C:\\Users\\rados\\Disk Google\\songs\\Stayin Out All Night.mp3", "Hip-Hop"));
         songList.add(new Song("So High", "Wiz Khalifa", 83, "C:\\Users\\rados\\Disk Google\\songs\\So High.mp3", "Hip-Hop"));
         songList.add(new Song("True Colors", "Wiz Khalifa", 73, "C:\\Users\\rados\\Disk Google\\songs\\True Colors.mp3", "Hip-Hop"));
     }
     
-    //Jeppe is khul
-   
     public ObservableList<Song> getAllSongs(){
         allSongs.addAll(songList);
         return allSongs;
